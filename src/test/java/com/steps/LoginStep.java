cf7a9aa84b1aeeb8703385d1757cda8a119a643e
linh09581 <linhnttpd09581@fpt.edu.vn>
2024-09-14 19:43:57 +0700

    Bai tap ve nha

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -1,6 +1,7 @@
 package com.steps;
 
 import com.utils.Utils;
+import config.Constants;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -10,10 +11,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import pages.HRMHomePage;
 import pages.HRMLoginPage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 public class LoginStep {
     public static final Logger logger = LogManager.getLogger();
@@ -24,15 +32,53 @@ public class LoginStep {
 
     WebDriver driver;
     @Given("I open web")
-    public void iOpenWeb(){
+    public void iOpenWeb() throws MalformedURLException {
 //        WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        String url = "https://icehrm-open.gamonoid.com/login.php";
+//        driver.get(url);
+//        Assert.assertEquals(driver.getCurrentUrl(), url);
+//        System.out.println(driver.getCurrentUrl());
+//        System.out.println("3333");
+        if (Constants.RUN_AT.equals("local")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            if (Constants.HEADLESS) {
+                options.addArguments("--headless");
+            }
+
+            options.addArguments("window-size=1920,1080");
+            options.addArguments("--no-sanbox");
+        } else  {
+            String hubURL = "https://hub.lambdatest.com/wd/hub";
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("browserName", "Chrome");
+            capabilities.setCapability("browserVersion", "95");
+            HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+            ltOptions.put("user", "linhbap2911");
+            ltOptions.put("accessKey", "XgH3dlUR8AtaRl3VjzYeG64L2fPAvcDk2hV8XcGb5Nh2VFFKZw");
+            ltOptions.put("build", "Selenium 4");
+            ltOptions.put("name", this.getClass().getName());
+            ltOptions.put("platformName", "Windows 10");
+            ltOptions.put("seCdp", true);
+            ltOptions.put("selenium_version", "4.0.0");
+            capabilities.setCapability("LT:Options", ltOptions);
+            driver = new RemoteWebDriver(new URL(hubURL), capabilities);
+        }
+
+
+
+//        driver = new ChromeDriver(options);
+
+
+        // Maximize the browser
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 30);
         String url = "https://icehrm-open.gamonoid.com/login.php";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        System.out.println(driver.getCurrentUrl());
-        System.out.println("3333");
     }
     @And("I click login")
     public void iClickLogin(){
